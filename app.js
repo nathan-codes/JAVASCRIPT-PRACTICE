3fff6b3f34711f0ecb71524b3afbecb6564120ec
Nathan Turkson <nathanturkson@gmail.com>
2023-03-26 21:56:53 +0000

    we go again

--- app.js
+++ app.js
@@ -1,37 +1,5 @@
-const todoItems = [];
-let userInput = "";
+const names = ["jessica", "davis"]
 
-
-while (userInput != "quit") {
-    userInput = prompt("What would you like to do: ");
-
-
-    if (userInput === "new") {
-        let todoItem = prompt("What would like to add: ");
-        todoItems.push(todoItem);
-        console.log(`${todoItem} added to list`)
-    }
-
-     if (userInput === "list") {
-        console.log("*********************")
-        for (let item of todoItems) {
-            console.log(`${todoItems.indexOf(item)}: ${item}`);
-        }
-        console.log("*********************")
-    }
-
-     if (userInput === "delete") {
-        let spliceIndex = parseInt((prompt("Enter the index of the todo Item you wish to delete: ")));
-        console.log(`${todoItems[spliceIndex]} has been deleted from the list`);
-        todoItems.splice(spliceIndex,1);
-    }
-
-
-
-
-
-
+for (let name of names) {
+    console.log(name);
 }
-
-
-console.log("OK! You quit the app!");
